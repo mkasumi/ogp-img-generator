94ed686178dae9cc5a74b6531456bbc6d418f359
mkasumi <morita@appleple.com>
2019-02-01 08:39:53 +0900

    Update: 背景アイコンを表示する

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,26 @@ window.onload = function(){
  });
 
  //ボタンを押下した際にダウンロードする画像を作る
-   html2canvas(document.getElementById("target")).then(function(canvas) {
+  html2canvas(document.getElementById("target")).then(function(canvas) {
       //aタグのhrefにキャプチャ画像のURLを設定
       const imgData = canvas.toDataURL();
       document.getElementById("ogp_image").href = imgData;
   });
 };
+
+var bg = function() {
+	var element = document.getElementsByClassName('js-ogp-bg')[0];
+
+	for(var i = 1; i <= 5; i++){
+    var ogpIcon = document.createElement( "span" );
+    var textNode = document.createTextNode("🐶");
+    ogpIcon.appendChild(textNode);
+    ogpIcon.setAttribute('class','c-img-ogp__icon')
+    element.appendChild(ogpIcon);
+}
+
+};
+
+window.addEventListener("load",function(){
+  bg();
+});
