c9014fd8092f48e46c01ccc5a1ec21e6e0e01c74
mkasumi <morita@appleple.com>
2020-07-18 01:19:27 +0900

    Fixed: 背景絵文字も変更時にプレビューに適用されるように修正

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,9 @@
 import html2canvas from '../../node_modules/html2canvas/dist/html2canvas.js';
 import Vue from '../../node_modules/vue/dist/vue.js';
 
-var genearatePreview = function() {
- var element = document.getElementsByClassName('js-ogp-bg')[0];
- var emoji = document.getElementsByClassName('js-ogp-emoji')[0];
- console.log('test' + emoji.dataset.emoji);
-
- for (var i = 0; i < 5; i++){
-  var ogpIconRow = document.createElement( "li" );
-  for (var j = 0; j < 5; j++){
-   var ogpIcon = document.createElement( "span" );
-   var textNode = document.createTextNode(emoji.dataset.emoji);
-   ogpIcon.appendChild(textNode);
-   ogpIcon.setAttribute('class','c-img-ogp__icon')
-   ogpIconRow.appendChild(ogpIcon);
-  }
-  element.appendChild(ogpIconRow);
- }
-};
 
 
 var generateImage = function() {
- console.log('generate!');
  html2canvas(document.getElementById("target")).then(function(canvas) {
   //aタグのhrefにキャプチャ画像のURLを設定
   const imgData = canvas.toDataURL();
@@ -30,12 +12,7 @@ var generateImage = function() {
 }
 
 window.addEventListener("load",function(){
- genearatePreview();
  generateImage();
-
- // document.getElementById("generateImage").onclick = function() {
- //  generateImage();
- // };
 });
 
 
@@ -44,7 +21,8 @@ new Vue({
  data: {
   newText: 'イヌかわいい',
   newTextColor: '#F45C63',
-  newEmoji: '🐶',
+  newEmoji: '🐶' +
+    '',
   newGradienColorStart: '#FFF',
   newGradienColorEnd: '#F45C63'
  },
